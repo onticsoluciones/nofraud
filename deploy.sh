1e0766d2e305c66349bec0879bec8b9f2d07c291
Ricardo <ricardo@ontic.es>
2016-12-03 23:40:34 +0100

    Modified dockerfiles for nofradu-data-sample

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#Magento Data dump example
+git clone https://github.com/onticsoluciones/nofraud-sample-data.git
+
 #Deploy 3 nodes (mysql, magento, nofraud)
 docker-compose up --build -d
 
