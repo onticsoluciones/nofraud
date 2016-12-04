c61a44bc363ac17930c250db691d786d9274ae64
Ricardo <ricardo@ontic.es>
2016-12-04 04:46:28 +0100

    Added sleep to deploy

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,7 @@ git clone https://github.com/onticsoluciones/nofraud-sample-data.git
 #Deploy 3 nodes (mysql, magento, nofraud)
 docker-compose up --build -d
 
+
 # Import SQL dump configured
+sleep 5
 docker exec -it magentonofraud sh /tmp/import.sh
